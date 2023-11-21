67a73523506bf9b53c83a5c6cca425838289d182
kjyjr <142625120+kjyjr@users.noreply.github.com>
2023-11-20 21:56:26 -0500

    final edits

--- EmployeeSQL/EmployeeSQL_table_schemata.sql
+++ EmployeeSQL/EmployeeSQL_table_schemata.sql
@@ -12,13 +12,13 @@ SELECT * FROM titles;
 DROP TABLE IF EXISTS employees CASCADE;
 
 CREATE TABLE employees(
-	emp_no INT,
+	emp_no INT NOT NULL,
 	emp_title_id VARCHAR,
 	birth_date VARCHAR,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex	VARCHAR,
-	hire_date DATE,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex	VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
@@ -29,8 +29,8 @@ SELECT * FROM employees;
 DROP TABLE IF EXISTS departments CASCADE;
 
 CREATE TABLE departments(
-	dept_no VARCHAR,
-	dept_name VARCHAR,
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
@@ -40,8 +40,8 @@ SELECT * FROM departments;
 DROP TABLE IF EXISTS dept_manager CASCADE;
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR,
-	emp_no INT,
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	PRIMARY KEY (dept_no, emp_no)
@@ -53,8 +53,8 @@ SELECT * FROM dept_manager;
 DROP TABLE IF EXISTS dept_emp CASCADE;
 
 CREATE TABLE dept_emp(
-	emp_no INT,
-	dept_no VARCHAR,
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
@@ -66,8 +66,8 @@ SELECT * FROM dept_emp;
 DROP TABLE IF EXISTS salaries CASCADE;
 
 CREATE TABLE salaries(
-	emp_no INT,
-	salary INT,
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
